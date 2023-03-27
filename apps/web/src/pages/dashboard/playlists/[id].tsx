d0b55226b21fcf80e282c0180beacea6f9b34852
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-03-27 22:56:08 +0200

    stuff

--- apps/web/src/pages/dashboard/playlists/[id].tsx
+++ apps/web/src/pages/dashboard/playlists/[id].tsx
@@ -128,19 +128,47 @@ const Questions = () => {
               <RadioGroup defaultValue="option-one">
                 <div className="flex items-center space-x-2">
                   <RadioGroupItem value="option-one" id="option-one" />
-                  <Input placeholder="Answer..." />
+                  <div className="relative w-full">
+                    <div className="absolute left-2 flex h-full items-center">
+                      <div className="flex h-5 w-5 rounded-md bg-slate-700 font-mono text-sm font-bold leading-none text-slate-200">
+                        <span className="m-auto">A</span>
+                      </div>
+                    </div>
+                    <Input placeholder="Answer..." className="pl-9" />
+                  </div>
                 </div>
                 <div className="flex items-center space-x-2">
                   <RadioGroupItem value="option-two" id="option-two" />
-                  <Input />
+                  <div className="relative w-full">
+                    <div className="absolute left-2 flex h-full items-center">
+                      <div className="flex h-5 w-5 rounded-md bg-slate-700 font-mono text-sm font-bold leading-none text-slate-200">
+                        <span className="m-auto">B</span>
+                      </div>
+                    </div>
+                    <Input className="pl-9" />
+                  </div>
                 </div>
                 <div className="flex items-center space-x-2">
                   <RadioGroupItem value="option-three" id="option-two" />
-                  <Input />
+                  <div className="relative w-full">
+                    <div className="absolute left-2 flex h-full items-center">
+                      <div className="flex h-5 w-5 rounded-md bg-slate-700 font-mono text-sm font-bold leading-none text-slate-200">
+                        <span className="m-auto">C</span>
+                      </div>
+                    </div>
+                    <Input className="pl-9" />
+                  </div>
                 </div>
                 <div className="flex items-center space-x-2">
                   <RadioGroupItem value="option-four" id="option-two" />
-                  <Input />
+                  <div className="relative w-full">
+                    <div className="absolute left-2 flex h-full items-center">
+                      <div className="flex h-5 w-5 rounded-md bg-slate-700 font-mono text-sm font-bold leading-none text-slate-200">
+                        <span className="m-auto">D</span>
+                      </div>
+                    </div>
+                    <Input className="pl-9" />
+                  </div>
                 </div>
               </RadioGroup>
             </div>
