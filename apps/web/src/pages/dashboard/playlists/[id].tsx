df096c69d8988b3d28157bfecb35ecf1d6f79803
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-03-29 00:22:05 +0200

    extract question form into new component

--- apps/web/src/pages/dashboard/playlists/[id].tsx
+++ apps/web/src/pages/dashboard/playlists/[id].tsx
@@ -22,8 +22,7 @@ import {
   SheetHeader,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { Textarea } from "@/components/ui/textarea";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { QuestionForm } from "@/components/question-form";
 
 const Settings = ({
   playlist,
@@ -38,8 +37,6 @@ const Settings = ({
       router.back();
     },
   });
-
-  console.log(playlist);
 
   const {
     handleSubmit,
@@ -117,61 +114,7 @@ const Questions = () => {
             <SheetHeader>New question</SheetHeader>
             <SheetDescription>Add a new question</SheetDescription>
             <Separator className="my-4" />
-            <div className="flex w-80 flex-col gap-4">
-              <div className="grid w-full gap-1.5">
-                <Label htmlFor="message">Question</Label>
-                <Textarea
-                  placeholder="What's the best programming language?"
-                  id="message"
-                />
-              </div>
-              <RadioGroup defaultValue="option-one">
-                <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="option-one" id="option-one" />
-                  <div className="relative w-full">
-                    <div className="absolute left-2 flex h-full items-center">
-                      <div className="flex h-5 w-5 rounded-md bg-slate-700 font-mono text-sm font-bold leading-none text-slate-200">
-                        <span className="m-auto">A</span>
-                      </div>
-                    </div>
-                    <Input placeholder="Answer..." className="pl-9" />
-                  </div>
-                </div>
-                <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="option-two" id="option-two" />
-                  <div className="relative w-full">
-                    <div className="absolute left-2 flex h-full items-center">
-                      <div className="flex h-5 w-5 rounded-md bg-slate-700 font-mono text-sm font-bold leading-none text-slate-200">
-                        <span className="m-auto">B</span>
-                      </div>
-                    </div>
-                    <Input className="pl-9" />
-                  </div>
-                </div>
-                <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="option-three" id="option-two" />
-                  <div className="relative w-full">
-                    <div className="absolute left-2 flex h-full items-center">
-                      <div className="flex h-5 w-5 rounded-md bg-slate-700 font-mono text-sm font-bold leading-none text-slate-200">
-                        <span className="m-auto">C</span>
-                      </div>
-                    </div>
-                    <Input className="pl-9" />
-                  </div>
-                </div>
-                <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="option-four" id="option-two" />
-                  <div className="relative w-full">
-                    <div className="absolute left-2 flex h-full items-center">
-                      <div className="flex h-5 w-5 rounded-md bg-slate-700 font-mono text-sm font-bold leading-none text-slate-200">
-                        <span className="m-auto">D</span>
-                      </div>
-                    </div>
-                    <Input className="pl-9" />
-                  </div>
-                </div>
-              </RadioGroup>
-            </div>
+            <QuestionForm />
             <Separator className="my-4" />
             <SheetFooter>
               <Button variant="ghost">Cancel</Button>
