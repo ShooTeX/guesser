14ca48cfe9b19010c46b1daddbf0e9358de8e63b
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-04-05 22:24:46 +0200

    stuff

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 // Set the paths that don't require the user to be signed in
-const publicPaths = ["/sign-in*", "/sign-up*"];
+const publicPaths = ["/", "/sign-in*", "/sign-up*"];
 
 const isPublic = (path: string) => {
   return publicPaths.find((x) =>
@@ -29,5 +29,5 @@ export default withClerkMiddleware((request: NextRequest) => {
 });
 
 export const config = {
-  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+  matcher: ["/((?!_next/image|_next/static|favicon.ico).*)", "/"],
 };
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,7 +1,24 @@
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { buildClerkProps, getAuth } from "@clerk/nextjs/server";
+import type { GetServerSideProps } from "next";
 import Link from "next/link";
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { userId } = getAuth(context.req);
+  if (userId) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: { ...buildClerkProps(context.req) } };
+};
 
 export default function Home() {
   return (
