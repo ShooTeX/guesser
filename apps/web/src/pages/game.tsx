82e8f5fada33c60e104f7a23b3aec3ea2d6649a3
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-04-07 02:06:21 +0200

    auth

--- apps/web/src/pages/game.tsx
+++ apps/web/src/pages/game.tsx
@@ -1,17 +1,31 @@
 import { Button } from "@/components/ui/button";
 import { api } from "@/lib/trpc";
+import { useUser } from "@clerk/nextjs";
 import { useState } from "react";
 
 export default function Game() {
+  const { user, isLoaded } = useUser();
   const [data, setData] = useState("");
-  api.game.onAdd.useSubscription(undefined, {
-    onData: (data) => setData(data.text),
-  });
+  api.game.onAdd.useSubscription(
+    { userId: user?.id || "", roomId: "asdf" },
+    {
+      onData: (data) => setData(data.user),
+      enabled: isLoaded,
+    }
+  );
   const mutation = api.game.add.useMutation();
   return (
     <div>
       {data}
-      <Button onClick={() => mutation.mutate({ text: "hello!" })}>Test</Button>
+      <Button
+        onClick={() =>
+          mutation.mutate({
+            text: (Math.random() + 1).toString(36).slice(7),
+          })
+        }
+      >
+        Test
+      </Button>
     </div>
   );
 }
--- packages/api/src/routers/game/_router.ts
+++ packages/api/src/routers/game/_router.ts
@@ -1,3 +1,4 @@
+import { clerkClient } from "@clerk/fastify";
 import { observable } from "@trpc/server/observable";
 import EventEmitter from "node:events";
 import { z } from "zod";
@@ -8,25 +9,32 @@ const ee = new EventEmitter();
 const postSchema = z.object({
   id: z.string(),
   text: z.string().min(1),
+  user: z.string(),
 });
 
 type Post = z.infer<typeof postSchema>;
 
 export const gameRouter = router({
-  onAdd: publicProcedure.subscription(() => {
-    return observable<Post>((emit) => {
-      const onAdd = (data: Post) => {
-        // emit data to client
-        emit.next(data);
-      };
+  onAdd: publicProcedure
+    .input(z.object({ roomId: z.string(), userId: z.string() }))
+    .subscription(async ({ input }) => {
+      const user = await clerkClient.users.getUser(input.userId);
+      return observable<Post>((emit) => {
+        const onAdd = (data: Post) => {
+          // emit data to client
+          emit.next({
+            ...data,
+            user: user.username || user.firstName || "null",
+          });
+        };
 
-      ee.on("add", onAdd);
+        ee.on("add", onAdd);
 
-      return () => {
-        ee.off("add", onAdd);
-      };
-    });
-  }),
+        return () => {
+          ee.off("add", onAdd);
+        };
+      });
+    }),
   add: publicProcedure
     .input(
       z.object({
