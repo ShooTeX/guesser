49eb4a5cdaaec0b1a6c14daf0f1012f796d2e1b3
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-04-10 23:39:04 +0200

    next playlist

--- apps/web/src/components/controls.tsx
+++ apps/web/src/components/controls.tsx
@@ -1,6 +1,8 @@
 import { api } from "@/lib/trpc";
 import { X } from "lucide-react";
 import { Button } from "./ui/button";
+import { PlaylistSelect } from "./playlist-select";
+import { useState } from "react";
 
 export type ControlsProperties = {
   onClose: () => void;
@@ -19,43 +21,67 @@ export const Controls = ({
   playlistName,
   roomId,
 }: ControlsProperties) => {
+  const [playlistSelectOpen, setPlaylistSelectOpen] = useState(false);
   const continueRoomMutation = api.game.continueRoom.useMutation();
+  const nextPlaylistMutation = api.game.nextPlaylist.useMutation();
   return (
-    <div className="overflow-hidden rounded-md border border-slate-100 dark:border-slate-600 dark:bg-slate-700">
-      <div className="flex justify-center bg-slate-100 py-1 px-4 dark:bg-slate-600">
-        <small className="flex-1 text-sm font-medium leading-none">
-          Host controls
-        </small>
-        <X className="h-4 w-4 cursor-pointer" onClick={() => onClose()} />
-      </div>
-      <div className="flex flex-col gap-4 px-4 py-3 text-sm">
-        <div className="grid w-64 grid-cols-3 gap-2">
-          <p>Playlist</p>
-          <p className="col-span-2 truncate text-right font-semibold">
-            {playlistName}
-          </p>
-          <p>Question</p>
-          <p className="col-span-2 text-right">
-            <span className="font-semibold">{currentQuestion + 1}</span> of{" "}
-            <span className="font-semibold">{questionsCount}</span>
-          </p>
-          <p>State</p>
-          <p className="col-span-2 truncate text-right font-semibold uppercase">
-            {state.replace("_", " ")}
-          </p>
+    <>
+      <PlaylistSelect
+        open={playlistSelectOpen}
+        onOpenChange={setPlaylistSelectOpen}
+        onSelect={(playlistId) => {
+          nextPlaylistMutation.mutate({ roomId, playlistId });
+          setPlaylistSelectOpen(false);
+        }}
+      />
+      <div className="overflow-hidden rounded-md border border-slate-100 dark:border-slate-600 dark:bg-slate-700">
+        <div className="flex justify-center bg-slate-100 py-1 px-4 dark:bg-slate-600">
+          <small className="flex-1 text-sm font-medium leading-none">
+            Host controls
+          </small>
+          <X className="h-4 w-4 cursor-pointer" onClick={() => onClose()} />
         </div>
-        <div className="flex flex-col gap-2">
-          <Button
-            className="w-full"
-            type="button"
-            onClick={() => {
-              continueRoomMutation.mutate({ id: roomId });
-            }}
-          >
-            Continue
-          </Button>
+        <div className="flex flex-col gap-4 px-4 py-3 text-sm">
+          <div className="grid w-64 grid-cols-3 gap-2">
+            <p>Playlist</p>
+            <p className="col-span-2 truncate text-right font-semibold">
+              {playlistName}
+            </p>
+            <p>Question</p>
+            <p className="col-span-2 text-right">
+              <span className="font-semibold">{currentQuestion + 1}</span> of{" "}
+              <span className="font-semibold">{questionsCount}</span>
+            </p>
+            <p>State</p>
+            <p className="col-span-2 truncate text-right font-semibold uppercase">
+              {state.replace("_", " ")}
+            </p>
+          </div>
+          <div className="flex flex-col gap-2">
+            {state === "end" ? (
+              <Button
+                className="w-full"
+                type="button"
+                onClick={() => {
+                  setPlaylistSelectOpen(true);
+                }}
+              >
+                Next playlist
+              </Button>
+            ) : (
+              <Button
+                className="w-full"
+                type="button"
+                onClick={() => {
+                  continueRoomMutation.mutate({ id: roomId });
+                }}
+              >
+                Continue
+              </Button>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
--- packages/api/src/routers/game/_router.ts
+++ packages/api/src/routers/game/_router.ts
@@ -3,10 +3,12 @@ import { continueRoom } from "./procedures/continue-room";
 import { createRoom } from "./procedures/create-room";
 import { guess } from "./procedures/guess";
 import { join } from "./procedures/join";
+import { nextPlaylist } from "./procedures/next-playlist";
 
 export const gameRouter = router({
   join,
   createRoom,
   continueRoom,
   guess,
+  nextPlaylist,
 });
