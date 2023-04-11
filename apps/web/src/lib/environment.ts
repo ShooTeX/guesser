c00b7a20e5dcc26d2c777ace98468946552a0a82
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-04-11 19:57:00 +0200

    fix

--- apps/web/src/lib/environment.ts
+++ apps/web/src/lib/environment.ts
@@ -1,9 +1,12 @@
 import { parseEnv, z } from "znv";
 
-export const { NODE_ENV, VERCEL_URL, API_URL } = parseEnv(process.env, {
-  NODE_ENV: z
-    .enum(["development", "test", "production"])
-    .default("development"),
-  VERCEL_URL: z.string().default("localhost:3000"),
-  API_URL: z.string().default("localhost:3001"),
-});
+export const { NODE_ENV, VERCEL_URL, NEXT_PUBLIC_API_URL } = parseEnv(
+  process.env,
+  {
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+    VERCEL_URL: z.string().default("localhost:3000"),
+    NEXT_PUBLIC_API_URL: z.string().default("localhost:3001"),
+  }
+);
