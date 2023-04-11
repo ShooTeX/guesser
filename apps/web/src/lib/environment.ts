6a86f8e04e104df439168eb68e4c094c71c5de09
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-04-11 19:08:00 +0200

    remove secret key from frontend

--- apps/web/src/lib/environment.ts
+++ apps/web/src/lib/environment.ts
@@ -1,8 +1,9 @@
 import { parseEnv, z } from "znv";
 
-export const { NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY, CLERK_SECRET_KEY, NODE_ENV } =
-  parseEnv(process.env, {
+export const { NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY, NODE_ENV } = parseEnv(
+  process.env,
+  {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
     NODE_ENV: z.enum(["development", "test", "production"]),
-    CLERK_SECRET_KEY: z.string(),
-  });
+  }
+);
