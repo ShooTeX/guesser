288fc9b94f81068659bf347f11522baf5e56dfb0
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-04-12 16:25:28 +0200

    esbuild

---
+++ apps/api/esbuild.ts
@@ -0,0 +1,17 @@
+import { build } from "esbuild";
+import esbuildPluginPino from "esbuild-plugin-pino";
+
+// eslint-disable-next-line unicorn/prefer-top-level-await
+void (async function () {
+  await build({
+    entryPoints: ["src/index.ts"],
+    logLevel: "info",
+    outdir: "dist",
+    minify: true,
+    bundle: true,
+    platform: "node",
+    format: "cjs",
+    sourcemap: true,
+    plugins: [esbuildPluginPino({ transports: ["pino-pretty"] })],
+  });
+})();
