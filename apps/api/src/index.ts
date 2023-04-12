5113e7c568a5e32ad7a01d9a4e2b22607ba12234
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-04-12 03:53:41 +0200

    use fastify cli

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
 import { fastifyTRPCPlugin } from "@trpc/server/adapters/fastify";
-import fastify from "fastify";
+import type { FastifyPluginAsync } from "fastify";
 import ws from "@fastify/websocket";
-import getPort from "get-port";
 import { clerkPlugin } from "@clerk/fastify";
 import cors from "@fastify/cors";
 import type { AppRouter as AppRouterType } from "./routers/_app";
@@ -12,39 +11,29 @@ import { createContext } from "./trpc/create-context";
 import { enableMapSet } from "immer";
 enableMapSet();
 
-const server = fastify({
-  maxParamLength: 5000,
-  logger: true,
-});
+// const server = fastify({
+//   maxParamLength: 5000,
+//   logger: true,
+// });
 
-void server.register(clerkPlugin);
+const app: FastifyPluginAsync = async (fastify) => {
+  await fastify.register(clerkPlugin);
 
-void server.register(ws);
+  await fastify.register(ws);
 
-void server.register(cors, {
-  origin: process.env.CLIENT_ORIGIN,
-  allowedHeaders: ["Authorization", "content-type"],
-});
+  await fastify.register(cors, {
+    origin: process.env.CLIENT_ORIGIN,
+    allowedHeaders: ["Authorization", "content-type"],
+  });
 
-void server.register(fastifyTRPCPlugin, {
-  prefix: "/trpc",
-  trpcOptions: { router: appRouter, createContext, onError: console.error },
-  useWSS: true,
-});
-
-// eslint-disable-next-line unicorn/prefer-top-level-await
-void (async () => {
-  try {
-    await server.listen({
-      host: "0.0.0.0",
-      port: await getPort({
-        port: [Number(process.env.PORT || 3000), 3001, 3002],
-      }),
-    });
-  } catch (error) {
-    server.log.error(error);
-    process.exit(1);
-  }
-})();
+  await fastify.register(fastifyTRPCPlugin, {
+    prefix: "/trpc",
+    trpcOptions: { router: appRouter, createContext, onError: console.error },
+    useWSS: true,
+  });
+};
 
 export type AppRouter = AppRouterType;
+
+export default app;
+export { app };
