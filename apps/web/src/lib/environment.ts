b255b8e9730eb2196086568c9bb75deff2caaf49
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-04-12 16:44:55 +0200

    remove znv

--- apps/web/src/lib/environment.ts
+++
@@ -1,9 +0,0 @@
-import { parseEnv, z } from "znv";
-
-export const { NODE_ENV, VERCEL_URL, API_URL } = parseEnv(process.env, {
-  NODE_ENV: z
-    .enum(["development", "test", "production"])
-    .default("development"),
-  VERCEL_URL: z.string().default("localhost:3000"),
-  API_URL: z.string().default("localhost:3001"),
-});
