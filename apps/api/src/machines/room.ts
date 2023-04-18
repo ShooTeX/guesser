a080e5dda08db13b53fffadf8b040e41aabdb146
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-04-18 22:18:30 +0200

    a lot of work

--- apps/api/src/machines/room.ts
+++ apps/api/src/machines/room.ts
@@ -1,13 +1,11 @@
 import type { roomSchema, playerSchema, answerSchema } from "@guesser/schemas";
 import { assign } from "@xstate/immer";
-import type { ActorRefFrom } from "xstate";
-import { spawn } from "xstate";
+import { maxBy } from "remeda";
 import { createMachine } from "xstate";
-import { sendParent } from "xstate/lib/actions";
+import { log, sendParent } from "xstate/lib/actions";
 import type { z } from "zod";
 import type { TwitchClient } from "../lib/twitch";
 import { activityMachine } from "./activity";
-import { predictionsMachine } from "./predictions";
 
 export const roomMachine = createMachine(
   {
@@ -18,8 +16,14 @@ export const roomMachine = createMachine(
     tsTypes: {} as import("./room.typegen").Typegen0,
     schema: {
       context: {} as z.infer<typeof roomSchema> & {
-        integrations: {
-          twitch?: ActorRefFrom<typeof predictionsMachine>;
+        twitch?: {
+          client: TwitchClient;
+          currentPrediction?: {
+            id: string;
+            outcomes: Awaited<
+              ReturnType<TwitchClient["createPrediction"]>
+            >["data"][0]["outcomes"];
+          };
         };
       },
       events: {} as
@@ -40,6 +44,17 @@ export const roomMachine = createMachine(
             questions: z.infer<typeof roomSchema>["questions"];
             playlistName: z.infer<typeof roomSchema>["playlistName"];
           },
+      services: {} as {
+        createPrediction: {
+          data: Awaited<ReturnType<TwitchClient["createPrediction"]>>["data"];
+        };
+        resolvePrediction: {
+          data: Awaited<ReturnType<TwitchClient["endPrediction"]>>["data"];
+        };
+        cancelPrediction: {
+          data: Awaited<ReturnType<TwitchClient["endPrediction"]>>["data"];
+        };
+      },
     },
     initial: "waiting",
     invoke: {
@@ -66,13 +81,67 @@ export const roomMachine = createMachine(
         },
       },
       showing_question: {
+        id: "showingQuestion",
         on: {
-          CONTINUE: {
-            target: "revealing_answer",
-          },
           GUESS: {
             actions: ["playerGuess"],
             cond: "playerDidNotGuess",
+          },
+        },
+        initial: "done",
+        always: { cond: "isTwitchEnabled", target: ".creating_prediction" },
+        states: {
+          done: {
+            on: {
+              CONTINUE: [
+                {
+                  target: "#showingQuestion.resolving_prediction",
+                  cond: "isTwitchEnabled",
+                },
+                {
+                  target: "#room.revealing_answer",
+                },
+              ],
+            },
+          },
+          creating_prediction: {
+            invoke: {
+              src: "createPrediction",
+              onDone: {
+                target: "#showingQuestion.done",
+                actions: "assignPrediction",
+              },
+              onError: {
+                target: "#showingQuestion.done",
+                actions: log(),
+              },
+            },
+          },
+          resolving_prediction: {
+            invoke: {
+              src: "resolvePrediction",
+              onDone: {
+                target: "#room.revealing_answer",
+                actions: ["setPredictionGuess", "resetCurrentPrediction"],
+              },
+              onError: {
+                target: "#showingQuestion.canceling_prediction",
+                actions: log(),
+              },
+            },
+          },
+          canceling_prediction: {
+            exit: "resetCurrentPrediction",
+            invoke: {
+              src: "cancelPrediction",
+              onDone: {
+                target: "#room.revealing_answer",
+              },
+              onError: {
+                target: "#room.revealing_answer",
+                actions: log(),
+              },
+            },
           },
         },
       },
@@ -124,14 +193,48 @@ export const roomMachine = createMachine(
   },
   {
     actions: {
-      // TODO: predictionsMachine is not stopped if integration is disabled
-      // keep this in mind if performance issues occur
+      resetCurrentPrediction: assign((context) => {
+        if (!context.twitch) return;
+        context.twitch.currentPrediction = undefined;
+      }),
+      setPredictionGuess: assign((context, event) => {
+        const currentQuestion = context.questions[context.currentQuestion];
+        if (!currentQuestion) return;
+
+        const [prediction] = event.data;
+        if (!prediction) return;
+
+        const player = context.players.find((player) => player.id === "TWITCH");
+        if (!player) return;
+
+        const outcomeWithMostPoints = maxBy(
+          prediction.outcomes,
+          (outcome) => outcome.channel_points
+        );
+
+        const answerId =
+          currentQuestion.answers[
+            prediction.outcomes.findIndex(
+              (outcome) => outcome.id === outcomeWithMostPoints?.id
+            )
+          ]?.id;
+
+        if (!answerId) return;
+
+        player.guess = answerId;
+      }),
+      assignPrediction: assign((context, event) => {
+        if (!context.twitch || !event.data[0]) return;
+
+        context.twitch.currentPrediction = {
+          id: event.data[0].id,
+          outcomes: event.data[0].outcomes,
+        };
+      }),
       setTwitchIntegration: assign((context, event) => {
         const player = context.players.find((player) => player.id === "TWITCH");
         if (event.value) {
-          context.integrations.twitch = spawn(
-            predictionsMachine.withContext({ client: event.value })
-          );
+          context.twitch = { client: event.value };
           if (!player) {
             context.players.push({
               connected: true,
@@ -147,8 +250,11 @@ export const roomMachine = createMachine(
             return;
           }
         }
-        if (!event.value && player && player) {
-          player.connected = false;
+        if (!event.value && player) {
+          context.twitch = undefined;
+          if (player) {
+            player.connected = false;
+          }
         }
       }),
       addPlayer: assign((context, event) => {
@@ -199,7 +305,66 @@ export const roomMachine = createMachine(
         }
       }),
     },
+    services: {
+      createPrediction: async (context) => {
+        const question = context.questions[context.currentQuestion];
+        if (!question) {
+          throw new Error("Question was not found");
+        }
+        const result = await context.twitch?.client.createPrediction({
+          title: question.question,
+          outcomes: question.answers.map((answer) => ({
+            title: answer.answer,
+          })),
+          prediction_window: 30,
+        });
+
+        if (!result) {
+          throw new Error("Could not create prediction");
+        }
+        return result.data;
+      },
+      resolvePrediction: async (context) => {
+        if (!context.twitch) throw new Error("twitch not enabled");
+        if (!context.twitch.currentPrediction)
+          throw new Error("No running predictions");
+
+        const correctAnswerIndex = context.questions[
+          context.currentQuestion
+        ]?.answers.findIndex((answer) => answer.correct);
+
+        if (!correctAnswerIndex || correctAnswerIndex === -1)
+          throw new Error("Correct answer could not be found");
+
+        const winning_outcome_id =
+          context.twitch.currentPrediction.outcomes[correctAnswerIndex]?.id;
+
+        if (!winning_outcome_id)
+          throw new Error("Outcome id could not be found");
+
+        const { data } = await context.twitch.client.endPrediction({
+          id: context.twitch.currentPrediction.id,
+          status: "RESOLVED",
+          winning_outcome_id,
+        });
+
+        return data;
+      },
+      cancelPrediction: async (context) => {
+        if (!context.twitch) throw new Error("twitch not enabled");
+        if (!context.twitch.currentPrediction)
+          throw new Error("No running predictions");
+
+        const { data } = await context.twitch.client.endPrediction({
+          id: context.twitch.currentPrediction.id,
+          status: "CANCELED",
+        });
+
+        return data;
+      },
+    },
     guards: {
+      isTwitchEnabled: ({ twitch }) => !!twitch,
       clientIsNotHost: ({ host }, event) =>
         host.id !== (event.type === "JOIN" ? event.player.id : event.id),
       playerExists: ({ players }, event) =>
