3acf68f6acc2a271cdb9a27bc7fec786a4b0fe3a
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-05-13 21:55:49 +0200

    lint

--- apps/web/src/components/ui/badge.tsx
+++ apps/web/src/components/ui/badge.tsx
@@ -1,7 +1,8 @@
-import * as React from "react"
-import { VariantProps, cva } from "class-variance-authority"
+import * as React from "react";
+import type { VariantProps } from "class-variance-authority";
+import { cva } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const badgeVariants = cva(
   "inline-flex items-center border rounded-full px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
@@ -21,16 +22,19 @@ const badgeVariants = cva(
       variant: "default",
     },
   }
-)
+);
 
-export interface BadgeProps
+export interface BadgeProperties
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof badgeVariants> {}
 
-function Badge({ className, variant, ...props }: BadgeProps) {
+function Badge({ className, variant, ...properties }: BadgeProperties) {
   return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
-  )
+    <div
+      className={cn(badgeVariants({ variant }), className)}
+      {...properties}
+    />
+  );
 }
 
-export { Badge, badgeVariants }
+export { Badge, badgeVariants };
